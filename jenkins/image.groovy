3c7fb681d40e0be85206a8e9d4e726dda07243d5
Lance Haynie <lhaynie@santaclarautah.gov>
2025-02-19 16:26:52 -0700

    jenkins

--- jenkins/image.groovy
+++ jenkins/image.groovy
@@ -13,8 +13,6 @@ withCredentials([usernamePassword(credentialsId: 'docker-hub', usernameVariable:
     echo "Using Commit Hash: $commit_hash for Docker build"
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-    rm -fR config/settings.yaml
-
     docker build --platform linux/x86_64 -t $image:$commit_hash-$branch --push .
 
     if [ $? -ne 0 ]; then
--- jenkins/tag.groovy
+++ jenkins/tag.groovy
@@ -10,9 +10,6 @@ withCredentials([usernamePassword(credentialsId: 'git', usernameVariable: 'GIT_U
 
     commit_hash=$(git rev-parse --short HEAD)
 
-    version_str=$commit_hash-$branch
-    yq -iY '.version = "'"$version_str"'"' spillman/version.yaml
-
     echo "Branch: ${branch} - Commit Hash: $commit_hash"
 
     git config --global user.email "jenkins@email.santaclarautah.gov"
